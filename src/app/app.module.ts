03a800d66a18be68bd707e42f8abbc8730f74d32
jbdura <bidurajohnson@gmail.com>
2023-07-22 01:12:08 +0300

    Done. Services and dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+// Components
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent} from "./nav/navbar.component";
 
+// Providers
+import { EventService } from "./events/shared/event.service";
+// import { ToastrServiceFeature} from "./common/toastr.service";
+
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
     NavBarComponent,
+
   ],
-  // providers: [],
+  providers: [
+    EventService,
+    // ToastrServiceFeature,
+  ],
   bootstrap: [ EventsAppComponent ]
 })
 export class AppModule { }
